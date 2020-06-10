97e85bac28220e17ec3c39ec2434100aea65cdf0
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-06-10 01:12:16 -0400

    Extension MKMapView added

--- FindMyWay/ViewController.swift
+++ FindMyWay/ViewController.swift
@@ -22,5 +22,17 @@ class ViewController: UIViewController {
 
 
 }
+
+private extension MKMapView {
+func centerToLocation(
+  _ location: CLLocation,
+  regionRadius: CLLocationDistance = 1000
+) {
+  let coordinateRegion = MKCoordinateRegion(
+    center: location.coordinate,
+    latitudinalMeters: regionRadius,
+    longitudinalMeters: regionRadius)
+  setRegion(coordinateRegion, animated: true)
+}
 }
 
