aebcca0fb4f3e4120d56c0f92db4a60337a33b68
Kritima Kukreja <kritimakukreja5@gmail.co>
2020-06-10 14:01:38 -0400

    Constraints added

--- FindMyWay/ViewController.swift
+++ FindMyWay/ViewController.swift
@@ -10,30 +10,48 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
     
     
     @IBOutlet weak var mapView: MKMapView!
-    let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+    var locationManager:CLLocationManager!
+       var currentLocationStr = "Current location"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.centerToLocation(initialLocation)
     }
+    override func viewDidAppear(_ animated: Bool) {
+        determineCurrentLocation()
+    }
+    
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+            let mUserLocation:CLLocation = locations[0] as CLLocation
+
+            let center = CLLocationCoordinate2D(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
+            let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+
+            mapView.setRegion(mRegion, animated: true)
+        }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print("Error - locationManager: \(error.localizedDescription)")
+        }
+    
+    func showOnMap(location: CLLocation )
+       {
+           let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+           let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+           mapView.setRegion(region, animated: true)
+       }
 
 
+func determineCurrentLocation() {
+    locationManager = CLLocationManager()
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    locationManager.requestAlwaysAuthorization()
+
+    if CLLocationManager.locationServicesEnabled() {
+        locationManager.startUpdatingLocation()
+    }
 }
-
-private extension MKMapView {
-func centerToLocation(
-  _ location: CLLocation,
-  regionRadius: CLLocationDistance = 1000
-) {
-  let coordinateRegion = MKCoordinateRegion(
-    center: location.coordinate,
-    latitudinalMeters: regionRadius,
-    longitudinalMeters: regionRadius)
-  setRegion(coordinateRegion, animated: true)
 }
-}
-
